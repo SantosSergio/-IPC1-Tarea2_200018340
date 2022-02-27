8fd04e78424664ee03a53cdead72b031d737e2f1
SantosSergio <sergioleonelsantosruano@outlook.com>
2022-02-27 12:17:48 -0600

    Primer commit

---
+++ src/Login/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Login;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+    public static void main(String[] args) {
+        new Ventana();
+        
+        
+        
+    }
+    
+}
